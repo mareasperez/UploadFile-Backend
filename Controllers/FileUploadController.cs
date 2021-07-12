d95a55ced7b02f69435f46f1b504ec0a01f8c947
Maury Areas <mareasperez7@gmail.com>
2021-07-11 19:07:01 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UploadFileExample.Models;
+
+namespace UploadFileExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        const string FILE_PATH = @"D:\Proyectos\C#\UploadFile\UploadFileExample\";
+        public FileUploadController()
+        {
+
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] FileToUpload theFile)
+        {
+            try
+            {
+                var filePathName = FILE_PATH + Path.GetFileNameWithoutExtension(theFile.FileName) + "-" +
+                    DateTime.Now.ToString().Replace("/", "").Replace(":", "").Replace(" ", "") +
+                    Path.GetExtension(theFile.FileName);
+                if (theFile.FileAsBase64.Contains(","))
+                {
+                    theFile.FileAsBase64 = theFile.FileAsBase64.Substring(theFile.FileAsBase64.IndexOf(",") + 1);
+                }
+                theFile.FileAsByteArray = Convert.FromBase64String(theFile.FileAsBase64);
+                using (var fs = new FileStream(filePathName, FileMode.CreateNew))
+                {
+                    fs.Write(theFile.FileAsByteArray, 0, theFile.FileAsByteArray.Length);
+                }
+                return Ok(new { messege = $"Imagen {filePathName} guardada" });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+
+}
